36eb83473b96d51808218f2f25cdcefa93245525
VishalBor <122140941+VishalBor@users.noreply.github.com>
2023-06-19 17:03:43 +0530

    practice

---
+++ spark/python file.py
@@ -0,0 +1,30 @@
+
+# '''#Combine two list and convert it into Dict
+# list1=["a","b","c","d","e","f"]
+# list2=[1,2,3,4,5,6]
+# New_Dict=dict(zip(list1,list2))
+# print(New_Dict)'''
+#
+#
+# ### find the element palindrome or not ###
+# my_list = ["level", "radar", "hello", "madam", "python", "racecar"]
+# palindrome_list = [word for word in my_list if word == word[::-1]]
+# print(palindrome_list)
+#
+# #### palindrone code ###
+# def isPalindrome(s):
+#     return s == s[::-1]
+#
+# # Driver code
+# s = "malayalam"
+# ans = isPalindrome(s)
+#
+# if ans:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+
+
